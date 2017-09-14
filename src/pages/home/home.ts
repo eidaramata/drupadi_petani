622d9e88f53f9235f502ab4080c0824b71f41409
eidaramata <fmunshi@eidaramata.com>
2017-09-14 17:44:06 +0700

    maps dan polygon klik to info page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,17 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef,NgZone } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { OneblokPage } from '../oneblok/oneblok';
+
+declare var google;
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  @ViewChild('map') mapElement: ElementRef;
+  map:any;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public ngZone:NgZone) {
 
   }
+  ionViewDidLoad(){
+    this.loadMap();
+  }
 blok(){
-  this.navCtrl.push(OneblokPage)
+  this.navCtrl.push(OneblokPage);
 }
+loadMap(){
+  let LatLng = new google.maps.LatLng(-6.895268, 107.54427);
+
+  let mapOptions = {
+    center:LatLng,
+    zoom:15,
+    disableDefaultUI: true,
+    mapTypeId: google.maps.MapTypeId.SATELLITE
+  };
+  this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+  var triangleCoords = [
+
+        {lat: -6.895268, lng: 107.54427},
+        {lat: -6.89302, lng: 107.544646},
+        {lat: -6.89399, lng: 107.547612},
+        {lat: -6.8958, lng: 107.545574},
+
+      ];
+
+      var bermudaTriangle = new google.maps.Polygon({
+         paths: triangleCoords,
+         strokeColor: '#FF0000',
+         strokeOpacity: 0.8,
+         strokeWeight: 2,
+         fillColor: '#FF0000',
+         fillOpacity: 0.35
+       });
+       bermudaTriangle.setMap(this.map);
+       google.maps.event.addListener(bermudaTriangle,'click', () => {
+         this.ngZone.run(()=>{
+           this.blok()
+         });
+        });
+  }
 }
--- src/pages/oneblok/oneblok.ts
+++ src/pages/oneblok/oneblok.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { MenuController,  NavController, NavParams } from 'ionic-angular';
 import { OneinfoPage } from '../oneinfo/oneinfo';
 import { OnetindakanPage } from '../onetindakan/onetindakan';
 import { OnekomentarPage } from '../onekomentar/onekomentar';
@@ -10,21 +10,17 @@ import { OnekomentarPage } from '../onekomentar/onekomentar';
  * Ionic pages and navigation.
  */
 
-@IonicPage()
 @Component({
-  selector: 'page-oneblok',
   templateUrl: 'oneblok.html',
 })
 export class OneblokPage {
+
   tab1Root = OneinfoPage;
   tab2Root = OnetindakanPage;
   tab3Root = OnekomentarPage;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
-
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad OneblokPage');
+  constructor(public navCtrl: NavController, public navParams: NavParams, public menu: MenuController) {
+        this.menu.swipeEnable(false);
   }
 
 }
